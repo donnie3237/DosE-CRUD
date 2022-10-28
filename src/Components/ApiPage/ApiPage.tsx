1a41a84b99c7341a238dec4a6fe87db87adcb208
SoRa <paradorn3237@gmail.com>
2022-10-28 10:06:24 +0700

    .

--- src/Components/ApiPage/ApiPage.tsx
+++ src/Components/ApiPage/ApiPage.tsx
@@ -19,14 +19,14 @@ function ApiPage() {
         <img src={require('./img/axios.png')}/>
         <div className="textt">
           <p>AXIOS คือการใช้คำสั่ง เพื่อส่งREquest ไปยังServer เพื่อGet ข้อมูลจากฐานข้อมูลมาแสดงบนหน้าเว็บของคุณณณณ</p>
-          <a href="https://www.javatpoint.com/react-axios#:~:text=In%20ReactJS%2C%20Axios%20is%20a%20library%20that%20serves,they%20can%20be%20normally%20shown%20on%20the%20website.">AXIOS</a>
+          <a href="https://www.javatpoint.com/react-axios#:~:text=In%20ReactJS%2C%20Axios%20is%20a%20library%20that%20serves,they%20can%20be%20normally%20shown%20on%20the%20website." target='blank'>AXIOS</a>
         </div>
       </div>
       <div className="api3" data-aos='fade-right'>
         <img src={require('./img/cors.png')}/>
         <div className="textt">
           <p>CORS คือการสร้างเว็บไซต์ที่สามารถใช้งานได้จากเว็บไซต์อื่นได้ หากคุณทำเว็บเซิร์ฟเวอร์ที่ใช้การดึงAPIแล้วไม่ติดตั้งตัวนี้ลงไป Error กำลังมองหาคุณอยู่</p>
-          <a href="">CORS</a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS">CORS</a>
         </div>
       </div>
     </div>
