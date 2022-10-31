7159a2e74453b18f6490058ea3b73d36365e88ab
SoRa <paradorn3237@gmail.com>
2022-10-31 20:26:06 +0700

    .

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+//นี่คือการอิมพอร์ทไลบรารีต่างๆ
 require('dotenv').config();
 const express = require('express')
 const app = express();
@@ -5,18 +6,21 @@ const PORT = process.env.PORT || 3030
 const cors = require('cors')
 const path = require('path')
 const mongoose = require('mongoose')
-const conNect = mongoose.connect()
 
+//นี่คือการเชื่อมฐานข้อมูลMongoBD
+
+//นี่คือมิดเดิ้ลแวร์ที่เชื่อม
 app.use(cors());
 
 
+//นี่คือส่วนของหารรันเซิร์ฟเวอร์บนพอร์ต
 app.listen(PORT,()=>
     {
         console.log("server is running on port",PORT)
     }
-    
-    
 )
+
+//นี่คือเราเตอร์ของเซิฟเวอร์(ของจิงต้องอิมพอร์ตเข้า เพราะมันมีเราเตอร์เยอะมากๆ)
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'./server.html'));
 })
