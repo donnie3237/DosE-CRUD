8a6b9644b617605df4799e038c436ac16f8505d9
SoRa <paradorn3237@gmail.com>
2022-11-26 11:08:21 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState,useEffect } from 'react';
 import './App.css';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './Components/Header/Header.tsx';
@@ -9,33 +8,12 @@ import Vdata from './Components/vdata/Vdata.tsx';
 import Create from './Components/Create/Create.tsx';
 import ApiPage from './Components/ApiPage/ApiPage.tsx';
 import DataPage from './Components/DataPage/DataPage.tsx';
-import HashLoader from 'react-spinners/HashLoader';
 import Detail from './Components/detail/Detail.tsx';
 import Edit from './Components/Edit/Edit.tsx';
 import DeletePage from './Components/DeletePage/DeletePage.tsx';
-
 function App() {
-  const [loading, setLoading] = useState(false)
-  useEffect(()=>{
-    setLoading(true);
-    setTimeout(()=>{
-      setLoading(false);
-    },2000)
-  },[])
-
   return (
     <div className="App">
-      {
-        loading ?
-        
-        <div className='hash'>
-        <HashLoader
-        color="#000000"
-        size={150}
-        speedMultiplier={1}
-        />
-        </div>
-        :
         <Router>
         <Header />
           <Routes>
@@ -50,10 +28,7 @@ function App() {
           </Routes>
         <Footer />
       </Router>
-      }
-      
     </div>
   );
 }
-
 export default App;
